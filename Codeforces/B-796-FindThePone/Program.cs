758bfc559fa8be5e1e443303bb9d301b7b6191fc
ahmed.mohamed <ahmed.m3.abdallah@gmail.com>
2020-10-19 14:24:40 +0200

    Add solution for problem B-796

---
+++ Codeforces/B-796-FindThePone/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace B_796_FindThePone
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] temp1 = Console.ReadLine().Split().Select(int.Parse).ToArray();
+ 
+            var holes = new HashSet<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+ 
+            int position = 1;
+            for (int i = 0; i < temp1[2]; i++)
+            {
+                int[] swap = Console.ReadLine().Split().Select(int.Parse).ToArray();
+                if (holes.Contains(position)) break;
+                if (position == swap[0]) position = swap[1];
+                else if (position == swap[1]) position = swap[0];
+            } 
+            
+            Console.WriteLine(position);
+        }
+    }
+}
