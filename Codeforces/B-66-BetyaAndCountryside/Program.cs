dbf34be74980377a270dac2268af64d24018e3a5
Ahmed Mohamed <ahmed.m3.abdallah@gmail.com>
2020-10-26 16:39:57 +0200

    Add a solution for problem Codeforces-B-66

---
+++ Codeforces/B-66-BetyaAndCountryside/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace B_66_BetyaAndCountryside
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            int[] sum = new int[n];
+            int c = 0;
+            for (int i = 0; i < n; i++)
+            {
+                if(i > 0 && arr[i] >= arr[i-1]) c++;
+                else c = 1;
+
+                sum[i] = c;
+            }
+
+            int mx = 0;
+            c = 0;
+            for (int i = n-2; i >= 0; i--)
+            {
+                if(arr[i] >= arr[i+1]) c++;
+                else c = 0;
+                mx = Math.Max(mx, c+sum[i]);
+            }
+            mx = Math.Max(mx, sum[n-1]);
+            Console.WriteLine(mx);
+        }
+    }
+}
